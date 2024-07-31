ef25abdced2b06f4dbd46301d71caf419fcdc5a9
aleksandranickij <janickiy@mail.ru>
2024-07-31 16:31:39 +0300

    seeder

---
+++ database/seeders/CommentTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comments;
+use App\Models\Posts;
+use Illuminate\Database\Seeder;
+
+class CommentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Posts::truncate();
+        Posts::unguard();
+        $faker = \Faker\Factory::create();
+        Posts::all()->each(function ($post) use ($faker) {
+            for ($i = 0; $i < 500; $i++)  {
+                Comments::create([
+                    'user_id' => $post->user_id,
+                    'comment'   => $faker->paragraphs(3, true),
+                    'commentable_id' => $post->id,
+                    'commentable_type' => 'posts',
+                ]);
+            }
+        });
+    }
+}
