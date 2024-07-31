6e85e85abc5798b96ddccbd1a9e7ca5fc8591007
aleksandranickij <janickiy@mail.ru>
2024-07-31 16:18:30 +0300

    bugfix

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::truncate();
+
+        User::create([
+            'name' => 'admin',
+            'login' => 'admin',
+            'password' => '1234567',
+            'role' => 'admin',
+        ]);
+
+        for ($i = 0; $i < 5000; ++$i) {
+            $faker = \Faker\Factory::create();
+            $password = bcrypt('secret');
+            User::create([
+                'name'     => $faker->name,
+                'email'    => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
